1219ba181df0cbde89500bfe7de06ad5f8504c19
732159 <732159@RH-TS-HP8000-03.district196.org>
2012-10-10 13:22:41 -0500

    included all lab 12

---
+++ java/src/count/count.java
@@ -0,0 +1,19 @@
+package count;
+import java.util.*;
+public class count {
+	public static void main (String args[])
+	{
+	System.out.println("enter numbers all in one line");
+	Scanner scan = new Scanner(System.in);
+	String str = scan.nextLine();
+	Scanner s = new Scanner(str);
+	String temp = " ";
+	int i = 0;
+	while (s.hasNext()){
+	temp=s.next();
+	i++;
+	}
+	System.out.print("there are " + i + " numbers");
+	
+}
+}
