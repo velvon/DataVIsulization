f7d6ae7e59f1bbc41647b801a6acac41256c76e5
Matthew <Matthew@192.168.47.39>
2012-10-22 13:16:31 -0500

    last array

---
+++ java/src/roman/RomanNumeral.java
@@ -0,0 +1,73 @@
+package roman;
+
+import static java.lang.System.*;
+
+public class RomanNumeral
+{
+	private Integer number;
+	private String roman;
+
+	private final static int[] NUMBERS= {1000,900,500,400,100,90,
+													50,40,10,9,5,4,1};
+
+	private final static String[] LETTERS = {"M","CM","D","CD","C","XC",
+												  "L","XL","X","IX","V","IV","I"};
+
+	public RomanNumeral(String str)
+	{
+
+		roman = str;
+
+	}
+
+	public RomanNumeral(Integer orig)
+	{
+
+		number = orig;
+
+	}
+
+	public void setNumber(Integer num)
+	{
+
+
+		number = num;
+
+
+	}
+
+	public void setRoman(String rom)
+	{
+
+		roman = rom;
+
+	}
+
+	public Integer getNumber()
+	{
+		if (number!=0)
+		return number;
+		
+			
+	}
+
+	public String toString()
+	{
+		int temp = number;
+		if (roman != null && roman!= "")
+		return roman + "\n";
+		while (temp>0)
+		{
+			for (int a = 0; a < 12; a++)
+			{
+				if(NUMBERS[a]<temp)
+				{
+					temp -= NUMBERS[a];
+					roman.concat(LETTERS[a]);
+				}
+				
+			}
+		}
+			return roman;
+	}
+}
