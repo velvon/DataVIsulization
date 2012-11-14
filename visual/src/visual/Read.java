9f06c6cec033f4ecc6b40d28ece67a12313df126
732159 <732159@RH-TS-HP8000-03.district196.org>
2012-11-14 13:24:32 -0600

    visual

--- visual/src/visual/Read.java
+++ visual/src/visual/Read.java
@@ -7,6 +7,7 @@ public class Read {
 	String st = " ";
 	int count=0;
 	int[] pas = new int[9];
+	int[] frm = new int[4];
 	Scanner scan;
 	
 	public Read() {
@@ -28,19 +29,20 @@ public class Read {
 
 	public void build() {
 	
-		
+		String t=" ";
 		int temp=0;
 	
 		while(scan.hasNext())
 		{
+			
 			if (scan.hasNextInt())
 			{
 				temp = scan.nextInt();
 			
 		if (temp<5)
-				pas[0]++;
+				break;
 		else if (temp<10)
-			pas[1]++;
+			break;
 		else if (temp<15)
 			pas[2]++;
 		else if (temp<20)
@@ -57,44 +59,80 @@ public class Read {
 			pas[8]++;
 		}
 			else
-				scan.next();
+			{
+			t=scan.next();
+			if (t.equalsIgnoreCase("Penitentiary,"))
+				frm[0]++;
+			if (t.equalsIgnoreCase("Workhouse"))
+				frm[1]++;
+			if (t.equalsIgnoreCase("Asylum"))
+				frm[2]++;
+			if (t.equalsIgnoreCase("Guardian"))
+				frm[3]++;
+			}
 		}
 
 	}
-	public int get5()
+//	public int get5()
+//	{
+//		return pas[0];
+//	}
+//	public int get10()
+//	{
+//		return pas[1];
+//	}
+//	public int get15()
+//	{
+//		return pas[2];
+//	}
+//	public int get20()
+//	{
+//		return pas[3];
+//	}
+//	public int get25()
+//	{
+//		return pas[4];
+//	}
+//	public int get30()
+//	{
+//		return pas[5];
+//	}
+//	public int get35()
+//	{
+//		return pas[6];
+//	}
+//	public int get40()
+//	{
+//		return pas[7];
+//	}
+//	public int get45()
+//	{
+//		return pas[8];
+//	}
+//	public int pen()
+//	{
+//		return frm[0];
+//	}
+//	public int work()
+//	{
+//		return frm[1];
+//	}
+//	public int asy()
+//	{
+//		return frm[2];
+//	}
+//	public int gua()
+//	{
+//		return frm[3];
+//	}
+	public int[] geta()
 	{
-		return pas[0];
+		return frm;
 	}
-	public int get10()
+	public int[] getnum()
 	{
-		return pas[1];
+		return pas;
 	}
-	public int get15()
-	{
-		return pas[2];
-	}
-	public int get20()
-	{
-		return pas[3];
-	}
-	public int get25()
-	{
-		return pas[4];
-	}
-	public int get30()
-	{
-		return pas[5];
-	}
-	public int get35()
-	{
-		return pas[6];
-	}
-	public int get40()
-	{
-		return pas[7];
-	}
-	public int get45()
-	{
-		return pas[8];
-	}
+	
+	
 }
